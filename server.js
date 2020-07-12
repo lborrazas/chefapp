28bee9a37b3601d92592d0715f9fef4ae9d73f06
Luis Mateo Borrazas <45957113+lborrazas@users.noreply.github.com>
2020-07-12 03:54:25 -0300

    redis sessions with config file

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const session = require('express-session');
+const redis = require('redis');
+const redisClient = redis.createClient();
+const redisStore = require('connect-redis')(session);
+var bodyParser = require('body-parser')
 
 
 
 const app = express()
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+app.use(urlencodedParser)
+
+redisClient.on('error', (err) => {
+    console.log('Redis error: ', err);
+});
 
 dotenv.config();
 
@@ -14,10 +26,48 @@ httpPort = process.env.APP_PORT;
 
 app.use(express.static(path.join(__dirname, 'www')))
 
+app.use(session({
+    secret: 'ThisIsHowYouUseRedisSessionStorage',
+    name: '_redisPractice',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Note that the cookie-parser module is no longer needed
+    store: new redisStore({ host: 'localhost', port: 6379, client: redisClient, ttl: 86400 }),
+}));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'www/index.html'))
 })
 
+
+app.post('/login',jsonParser, function(req,res){
+    // when user login set the key to redis.
+    console.log(req.body.email)
+    req.session.key=req.body.email;
+    res.end('done');
+});
+
+app.get('/logout',function(req,res){
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+
 app.listen(httpPort, function () {
     console.log(`Listening on port ${httpPort}!`)
 })
+
+
+// Start a session; we use Redis for the session store.
+// "secret" will be used to create the session ID hash (the cookie id and the redis key value)
+// "name" will show up as your cookie name in the browser
+// "cookie" is provided by default; you can add it to add additional personalized options
+// The "store" ttl is the expiration time for each Redis session ID, in seconds
+
+
+
