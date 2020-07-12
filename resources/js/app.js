39eaadb3bce166e26572f417151fa8a8e5fc2a77
Luis Mateo Borrazas <45957113+lborrazas@users.noreply.github.com>
2020-07-12 01:33:43 -0300

    vue components for core and complejito for juan

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 window.Vue = require('vue')
 
 Vue.component('first-component', require('./components/firstComponent.vue').default);
+Vue.component('tab-bar-compnent', require('./components/tabBarComponent.vue').default);
+Vue.component('mui-panel-component', require('./components/muiPanelComponent.vue').default);
+Vue.component('mui-page-component', require('./components/pageComponent.vue').default);
+Vue.component('chef-page-component', require('./components/pagesComponents/paginaDelChef/chefPage.vue').default);
+
+
 
 
 const app = new Vue({
@@ -11,5 +17,6 @@ const app = new Vue({
     methods: {},
     mounted() {
     },
+
 })
 
