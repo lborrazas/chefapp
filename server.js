b03fffcd85facf61fa154b43316651fc11a8391f
Luis Mateo Borrazas <45957113+lborrazas@users.noreply.github.com>
2020-07-13 03:50:40 -0300

    reorder

--- server.js
+++ server.js
@@ -17,78 +17,70 @@ var urlencodedParser = bodyParser.urlencoded({extended: false})
 dotenv.config();
 
 const app = express()
+redisClient.on('error', (err) => {
+    console.log('Redis error: ', err);
+});
 
+let httpPort = process.env.APP_PORT;
+let database = 'chefsapp';
+let client = db.connect();
 
+app.use(urlencodedParser)
 
-    app.use(urlencodedParser)
+app.use(express.static(path.join(__dirname, 'www')))
 
-    redisClient.on('error', (err) => {
-        console.log('Redis error: ', err);
-    });
+app.use(session({
+    secret: 'ThisIsHowYouUseRedisSessionStorage',
+    name: 'REDIS-SESSION',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}, // Note that the cookie-parser module is no longer needed
+    store: new redisStore({host: 'localhost', port: 6379, client: redisClient, ttl: 86400}),
+}));
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'www/index.html'))
+})
 
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    next();
+});
 
-    let httpPort = process.env.APP_PORT;
-    let database = 'chefsapp';
+app.post('/login', jsonParser, function (req, res) {
+    // when user login set the key to redis.
+    console.log(req.body.email)
+    req.session.key = req.body.email;
+    res.end('done');
+});
 
-    app.use(express.static(path.join(__dirname, 'www')))
-
-    app.use(session({
-        secret: 'ThisIsHowYouUseRedisSessionStorage',
-        name: 'REDIS-SESSION',
-        resave: false,
-        saveUninitialized: true,
-        cookie: {secure: false}, // Note that the cookie-parser module is no longer needed
-        store: new redisStore({host: 'localhost', port: 6379, client: redisClient, ttl: 86400}),
-    }));
-
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, 'www/index.html'))
-    })
-
-
-    let client = db.connect();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use((req, res, next) => {
-        res.set('Access-Control-Allow-Origin', '*');
-        res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-        next();
-    });
-
-
-    app.post('/login', jsonParser, function (req, res) {
-        // when user login set the key to redis.
-        console.log(req.body.email)
-        req.session.key = req.body.email;
-        res.end('done');
-    });
-
-    app.get('/logout', function (req, res) {
-        req.session.destroy(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.redirect('/');
-            }
-        });
-    });
-
-    app.post('/users', async (req, res) => {
-        let collection = 'usuarios';
-        try {
-            await db.insertOne(client, database, collection, req.body);
-            res.status(200).end();
-        } catch (err) {
+app.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
             console.log(err);
-            res.status(400).end();
+        } else {
+            res.redirect('/');
         }
     });
+});
 
-    app.use('/api', apiRouter);
+app.post('/users', async (req, res) => {
+    let collection = 'usuarios';
+    try {
+        await db.insertOne(client, database, collection, req.body);
+        res.status(200).end();
+    } catch (err) {
+        console.log(err);
+        res.status(400).end();
+    }
+});
 
-    app.listen(httpPort, function () {
-        console.log(`Listening on port ${httpPort}!`)
-    });
+app.use('/api', apiRouter);
+
+app.listen(httpPort, function () {
+    console.log(`Listening on port ${httpPort}!`)
+});
 
 
