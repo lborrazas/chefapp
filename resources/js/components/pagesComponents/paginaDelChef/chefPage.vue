f769ac36ba6154e1abef9bbdbf6642d10460903a
jabuarab <alasjan1@gmail.com>
2020-07-26 01:30:26 -0300

    mergeado

--- resources/js/components/pagesComponents/paginaDelChef/chefPage.vue
+++ resources/js/components/pagesComponents/paginaDelChef/chefPage.vue
@@ -2,19 +2,7 @@
     <page-template page_identification="chef-page">
         <template slot="page-title"> La Pag del Chef</template>
         <template slot="page-body">
-            <complejito v-on:juanito-evento="llamarGrande"></complejito>
 
-
-            <button-keen @click="$refs['modal'].open()">ABRIR</button-keen>
-            <button @click="llamadaDeNacho"></button>
-
-            <h1 v-show="isLoading">ESTOY CARGANDO LA CONCHA DE TU MADRE</h1>
-
-
-            <modal-two ref="modal" title="Ui como estas">
-                <h1>JUAN ES EL PROPIO</h1>
-            </modal-two>
-            <h1>HELO</h1>
 
         </template>
     </page-template>
@@ -32,15 +20,11 @@
         name: "chefPage",
         data() {
             return {
-                chef: {},
-                isLoading: false,
+
             }
         },
         components: {
-            'page-template': pageComponent,
-            'complejito': complejito,
-            'modal-two': UiModal,
-            'button-keen': UiButton,
+
         },
         methods: {
 
